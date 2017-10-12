6ce253d35ce081a1fc1d5b2880d825d6bf825d96
nachmadi <skatir_dev@yahoo.com>
2017-10-12 14:37:39 +0700

    create component

---
+++ client/hacktivpress/src/components/FooterBlog.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div class="container-fluid text-center">
+    <p>Footer Text</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ client/hacktivpress/src/components/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <HeaderBlog/>
+    <Sidebar/>
+    <ContentBlog/>
+    <FooterBlog/>
+  </div>
+</template>
+
+<script>
+import HeaderBlog from '@/components/HeaderBlog'
+import Sidebar from '@/components/Sidebar'
+import ContentBlog from '@/components/ContentBlog'
+import FooterBlog from '@/components/FooterBlog'
+export default {
+  components: {
+    HeaderBlog,
+    Sidebar,
+    ContentBlog,
+    FooterBlog
+  }
+}
+</script>
